f008febe3916c5288bb374615f36ee5aaac60cdd
sownt <thonsai@outlook.com>
2025-04-04 15:39:30 +0700

    fix seo

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,95 +1,23 @@
 import { ImageResponse } from "next/og";
-import { NextRequest } from "next/server";
 
 export const runtime = "edge";
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = req.nextUrl;
-  const postTitle = searchParams.get("title") || "GDG Cloud Hanoi";
-  const font = fetch(
-    new URL(`${process.env.NEXT_PUBLIC_SITE_URL}/fonts/GoogleSans-Medium.ttf`, import.meta.url)
-  ).then((res) => res.arrayBuffer());
-  const fontData = await font;
-
+export async function GET() {
   return new ImageResponse(
     (
-      <div
+      <img
+        src={`${process.env.NEXT_PUBLIC_SITE_URL}/preview.jpg`}
+        width={900}
         style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "#fff",
-          backgroundImage: `url(${process.env.NEXT_PUBLIC_SITE_URL}/og.png)`,
-          fontSize: 32,
-          fontWeight: 600,
+          position: "relative",
+          bottom: -160,
+          aspectRatio: "auto",
+          border: "4px solid lightgray",
+          background: "lightgray",
+          borderRadius: 20,
+          zIndex: 1,
         }}
-      >
-        <div
-          style={{
-            position: "relative",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            top: "125px",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              fontSize: "64px",
-              fontWeight: "600",
-              marginTop: "24px",
-              textAlign: "center",
-              width: "80%",
-              letterSpacing: "-0.05em",
-            }}
-          >
-            {postTitle}
-          </div>
-          <div
-            style={{
-              display: "flex",
-              fontSize: "16px",
-              fontWeight: "500",
-              marginTop: "16px",
-              color: "#808080",
-            }}
-          >
-            Example Site
-          </div>
-        </div>
-
-        <img
-          src={`${process.env.NEXT_PUBLIC_SITE_URL}/dashboard.png`}
-          width={900}
-          style={{
-            position: "relative",
-            bottom: -160,
-            aspectRatio: "auto",
-            border: "4px solid lightgray",
-            background: "lightgray",
-            borderRadius: 20,
-            zIndex: 1,
-          }}
-        />
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: "Inter",
-          data: fontData,
-          style: "normal",
-        },
-      ],
-    }
+      />
+    )
   );
 }
